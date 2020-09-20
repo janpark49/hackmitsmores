4d197aaee71eb5231f8a7f3cf9228d57a39e0077
Kartikesh314 <67815239+Kartikesh314@users.noreply.github.com>
2020-09-20 19:29:51 +0545

    Delete qpage.py

--- smores_app/qpage.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-global data=[]
-@app.route()
-def qpage():
-    return render_template("qpage.html", ams = data[0], win=data[1], lose= data[2], imglnk=data[3], quest=data[4], ams1=data[5], ams2=data[6], ams3=data[7], ams4=data[8])
-
-if __name__ == '__main__':
-    app.run()
